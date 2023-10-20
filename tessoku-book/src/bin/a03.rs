8024d1b82bf470238191b2ffca139ec038ecd276
yoshi47 <yoshix47@gmail.com>
2023-10-20 11:50:01 +0900

    1.3 全探索（2）

--- tessoku-book/src/bin/a03.rs
+++ tessoku-book/src/bin/a03.rs
@@ -1,4 +1,18 @@
 use proconio::input;
 fn main() {
-    input! {}
+    input! {
+        n: i32,
+        k: i32,
+        p: [i32; n],
+        q: [i32; n],
+    }
+    for pp in &p {
+        for qq in &q {
+            if pp + qq == k {
+                println!("Yes");
+                return;
+            }
+        }
+    }
+    println!("No")
 }
--- tessoku-book/src/bin/b03.rs
+++ tessoku-book/src/bin/b03.rs
@@ -1,4 +1,19 @@
 use proconio::input;
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    for i in 0..n {
+        for j in i + 1..n {
+            for k in j + 1..n {
+                if a[i] + a[j] + a[k] == 1000 {
+                    println!("Yes");
+                    return;
+                }
+            }
+        }
+    }
+    println!("No");
 }
