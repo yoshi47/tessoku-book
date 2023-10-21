e302bc5183d5879d8dcacf9c7286eaa9aeaaa272
yoshi47 <yoshix47@gmail.com>
2023-10-21 11:38:34 +0000

    1.4 ２進数 １０進数以外に慣れたい

--- tessoku-book/src/bin/a04.rs
+++ tessoku-book/src/bin/a04.rs
@@ -1,4 +1,16 @@
 use proconio::input;
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+    }
+
+    let nlen = { format!("{:b}", 1000).to_string().len() };
+    for i in (0..nlen).rev() {
+        let wari: usize = 2usize.pow(i as u32);
+        print!("{}", (n / wari) % 2);
+    }
+    println!();
+
+
+    // println!("{:010b}", n);
 }
--- tessoku-book/src/bin/b04.rs
+++ tessoku-book/src/bin/b04.rs
@@ -1,4 +1,12 @@
 use proconio::input;
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+    }
+
+    let ans = usize::from_str_radix(&n.to_string(), 2);
+    match ans {
+        Ok(x) => println!("{}", x),
+        Err(_e) => return,
+    }
 }
