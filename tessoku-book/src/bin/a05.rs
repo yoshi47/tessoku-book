43bdad7b512b8bcd3545fad948deb9775cbd043d
yoshi47 <yoshix47@gmail.com>
2023-10-23 07:12:02 +0000

    1.5 全探索と計算量

--- tessoku-book/src/bin/a05.rs
+++ tessoku-book/src/bin/a05.rs
@@ -1,4 +1,17 @@
 use proconio::input;
 fn main() {
-    input! {}
+    input! {
+        n: i64,
+        k: i64,
+    }
+    let mut ans = 0;
+    for i in 1..=n {
+        for j in 1..=n {
+            let x = k - (i + j);
+            if 1 <= x && x <= n {
+                ans += 1;
+            }
+        }
+    }
+    println!("{}", ans);
 }
