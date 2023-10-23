0c28bde666df9683c000b1bb8ec8e491a09fb77b
yoshi47 <yoshix47@gmail.com>
2023-10-23 08:43:41 +0000

    2.1 一次元の累積和（1） rustの型にちゅーい

--- tessoku-book/src/bin/a06.rs
+++ tessoku-book/src/bin/a06.rs
@@ -1,4 +1,18 @@
 use proconio::input;
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        q: usize,
+        a: [i32; n],
+        qq: [[i32;2]; q],
+    }
+
+    let mut s = vec![0_i32; n+1];
+    for i in 1..n+1 {
+        s[i] = s[i-1] + a[i-1];
+    }
+
+    for i in 0..q {
+        println!("{}", s[qq[i][1] as usize] - s[qq[i][0] as usize - 1]);
+    }
 }
--- tessoku-book/src/bin/b06.rs
+++ tessoku-book/src/bin/b06.rs
@@ -1,4 +1,24 @@
 use proconio::input;
 fn main() {
-    input! {}
+    input! {
+        a: [usize],
+        q: [(usize, usize)],
+    }
+
+    let mut s = vec![0_usize; a.len()+1];
+    for i in 0..a.len() {
+        s[i+1] = s[i] + a[i];
+    }
+
+    for (l, r) in q {
+        let kachi = (s[r] - s[l-1]) as f32;
+        let do_num = (r - (l-1)) as f32;
+        if kachi > do_num / 2.0 {
+            println!("win");
+        } else if kachi  < do_num / 2.0 {
+            println!("lose");
+        } else {
+            println!("draw");
+        }
+    }
 }
