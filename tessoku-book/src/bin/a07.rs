e6daa8ac5db45124aa184fd23deabfbff4f1fd4a
yoshi47 <yoshix47@gmail.com>
2023-10-24 07:34:49 +0000

    2.2 一次元の累積和（2） 配列の長さに気をつける

--- tessoku-book/src/bin/a07.rs
+++ tessoku-book/src/bin/a07.rs
@@ -1,4 +1,18 @@
 use proconio::input;
 fn main() {
-    input! {}
+    input! {
+        d: usize,
+        p: [(usize, usize)],
+    }
+    let mut b = vec![0_i32; d+1];
+    for (l, r) in p {
+        b[l-1] += 1;
+        b[r] -= 1;
+    }
+
+    let mut ans = 0;
+    for i in 0..d {
+        ans += b[i];
+        println!("{}", ans);
+    }
 }
--- tessoku-book/src/bin/b07.rs
+++ tessoku-book/src/bin/b07.rs
@@ -1,4 +1,19 @@
 use proconio::input;
 fn main() {
-    input! {}
+    input! {
+        t: usize,
+        p: [(usize, usize)],
+    }
+    let mut b = vec![0; t+1];
+
+    for (l, r) in p {
+        b[l] += 1;
+        b[r] -= 1;
+    }
+
+    let mut ans = 0;
+    for i in 0..t {
+        ans += b[i];
+        println!("{ans}");
+    }
 }
