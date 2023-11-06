a97354def392bc68a89c7167b0c597f3a195da8e
yoshi47 <yoshix47@gmail.com>
2023-11-06 10:57:31 +0000

    2.3 二次元の累積和（1）

--- tessoku-book/src/bin/a08.rs
+++ tessoku-book/src/bin/a08.rs
@@ -1,4 +1,27 @@
 use proconio::input;
 fn main() {
-    input! {}
+    input! {
+        h: usize,
+        w: usize,
+        x: [[usize; w]; h],
+        q: [(usize, usize, usize, usize)],
+    }
+    let mut z = vec![vec![0_usize; w+1]; h+1];
+
+    for i in 1..h+1 {
+        for j in 1..w+1 {
+            z[i][j] = z[i][j-1] + x[i-1][j-1];
+        }
+    }
+    for j in 1..w+1 {
+        for i in 1..h+1 {
+            z[i][j] = z[i-1][j] + z[i][j];
+        }
+    }
+    // println!("{:?}", z);
+
+    for (a, b, c, d) in q {
+        let ans = z[c][d] + z[a-1][b-1] - z[c][b-1] - z[a-1][d];
+        println!("{ans}");
+    }
 }
--- tessoku-book/src/bin/b08.rs
+++ tessoku-book/src/bin/b08.rs
@@ -1,4 +1,29 @@
 use proconio::input;
 fn main() {
-    input! {}
+    input! {
+        xy: [(usize, usize)],
+        q : [(usize, usize, usize, usize)],
+    }
+    let mut z = vec![vec![0_usize; 1500]; 1500];
+    let mut k = vec![vec![0_usize; 1501]; 1501];
+
+    for (x, y) in xy {
+        z[x-1][y-1] += 1;
+    }
+
+    for i in 1..1501 {
+        for j in 1..1501 {
+            k[i][j] = k[i][j-1] + z[i-1][j-1];
+        }
+    }
+    for j in 1..1501 {
+        for i in 1..1501 {
+            k[i][j] += k[i-1][j];
+        }
+    }
+
+    for (a, b, c, d) in q {
+        let ans:usize = k[c][d] + k[a-1][b-1] - k[c][b-1] - k[a-1][d];
+        println!("{ans}");
+    }
 }
