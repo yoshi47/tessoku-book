3c1faf179436df6214542a81811e11f56ecedae4
yoshi47 <yoshix47@gmail.com>
2023-11-07 08:52:08 +0000

    2.4 二次元の累積和（2）

--- tessoku-book/src/bin/a09.rs
+++ tessoku-book/src/bin/a09.rs
@@ -1,4 +1,35 @@
 use proconio::input;
 fn main() {
-    input! {}
+    input! {
+        h: usize,
+        w: usize,
+        n: usize,
+        z: [(usize, usize, usize, usize); n],
+    }
+    let mut zz = vec![vec![0_i32; w+1]; h+1];
+    for (a, b, c, d) in z {
+        zz[a-1][b-1] += 1;
+        zz[c][d] += 1;
+        zz[a-1][d] -= 1;
+        zz[c][b-1] -= 1;
+    }
+    // 累積和の計算
+    let mut zzz = vec![vec![0_i32; w+2]; h+2];
+    for i in 1..h+1 {
+        for j in 1..w+1 {
+            zzz[i][j] = zzz[i][j-1] + zz[i-1][j-1];
+        }
+    }
+    for j in 1..w+1 {
+        for i in 1..h+1 {
+            zzz[i][j] = zzz[i-1][j] + zzz[i][j];
+        }
+    }
+
+    for i in 1..h+1 {
+        for j in 1..w+1 {
+            print!("{} ", zzz[i][j]);
+        }
+        println!();
+    }
 }
--- tessoku-book/src/bin/b09.rs
+++ tessoku-book/src/bin/b09.rs
@@ -1,4 +1,40 @@
 use proconio::input;
 fn main() {
-    input! {}
+    input! {
+        z: [(usize, usize, usize, usize)],
+    }
+    let mut zz = vec![vec![0_isize; 1502]; 1502];
+    for (a, b, c, d) in z {
+        zz[a][b] += 1;
+        zz[c][d] += 1;
+        zz[a][d] -= 1;
+        zz[c][b] -= 1;
+    }
+
+    let mut map_n = vec![vec![0_isize; 1503]; 1503];
+    for i in 1..1503 {
+        for j in 1..1503 {
+            map_n[i][j] = map_n[i][j-1] + zz[i-1][j-1];
+        }
+    }
+    for j in 1..1503 {
+        for i in 1..1503 {
+            map_n[i][j] += map_n[i-1][j];
+        }
+    }
+    // println!("{:?}", map_n);
+    // let ans: usize = map_n
+    // .iter()
+    // .map(|row| row.iter().filter(|&&value| value >= 1).count())
+    // .sum();
+    // println!("{ans}");
+    let mut ans = 0;
+    for i in 0..1503 {
+        for j in 0..1503 {
+            if map_n[i][j] >= 1 {
+                ans += 1;
+            }
+        }
+    }
+    println!("{ans}");
 }
