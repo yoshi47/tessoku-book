1eb2c78e99a86039da6cb6b32a70bf9663808877
yoshi47 <yoshix47@gmail.com>
2023-11-08 03:23:05 +0000

    2.5 チャレンジ

--- tessoku-book/src/bin/a10.rs
+++ tessoku-book/src/bin/a10.rs
@@ -1,4 +1,26 @@
 use proconio::input;
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        a: [usize; n],
+        b: [(usize, usize)],
+    }
+    let mut p = vec![0_usize; n];
+    let mut q = vec![0_usize; n];
+
+    p[0] += a[0];
+    for i in 1..n {
+        p[i] = p[i-1].max(a[i]);
+    }
+
+    q[n-1] += a[n-1];
+    for i in (0..n-1).rev() {
+        q[i] = q[i+1].max(a[i]);
+    }
+
+    for (l, r) in b {
+        let ans = std::cmp::max(p[l-2], q[r]);
+        println!("{}", ans);
+    }
+
 }
