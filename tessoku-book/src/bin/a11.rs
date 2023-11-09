a4e26066dddde4b51d1cdd86487682bc93ed62c9
yoshi47 <yoshix47@gmail.com>
2023-11-09 02:06:40 +0000

    3.1 配列の二分探索

--- tessoku-book/src/bin/a11.rs
+++ tessoku-book/src/bin/a11.rs
@@ -1,4 +1,26 @@
 use proconio::input;
+
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        x: usize,
+        a: [usize; n],
+    }
+    // let mut ans = 0;
+    // let (mut l, mut r) = (0, n);
+    // while l <= r {
+    //     let m = (l + r) / 2;
+    //     if x < a[m] {
+    //         r = m -1;
+    //     }
+    //     if x == a[m] {
+    //         ans = m + 1;
+    //         break
+    //     }
+    //     if x > a[m] {
+    //         l = m + 1;
+    //     }
+    // }
+    let ans = a.binary_search(&x).unwrap() + 1;
+    println!("{ans}");
 }
--- tessoku-book/src/bin/b11.rs
+++ tessoku-book/src/bin/b11.rs
@@ -1,4 +1,14 @@
 use proconio::input;
 fn main() {
-    input! {}
+    input! {
+        mut a: [usize],
+        x: [usize],
+    }
+    a.sort();
+    for i in x {
+        match a.binary_search(&i) {
+            Ok(ans) => println!("{ans}"),
+            Err(ans) => println!("{ans}"),
+        }
+    }
 }
