699f3d5a36d305a6e10c7adcac0f86030170fd40
yoshi <yoshix47@gmail.com>
2023-12-30 10:20:11 +0900

    3.2 答えで二分探索

--- tessoku-book/src/bin/a12.rs
+++ tessoku-book/src/bin/a12.rs
@@ -1,4 +1,25 @@
 use proconio::input;
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize; n],
+    }
+
+    let mut ans = 0;
+    let (mut l, mut r) = (0, 1000000000);
+    while l < r {
+        let m = (l + r) / 2;
+        let mut sum = 0;
+        for i in 0..n {
+            sum += m / a[i];
+        }
+
+        if k <= sum {
+            r = m;
+        }  else {
+            l = m + 1;
+        }
+    }
+    println!("{l}")
 }
--- tessoku-book/src/bin/b12.rs
+++ tessoku-book/src/bin/b12.rs
@@ -1,4 +1,17 @@
 use proconio::input;
 fn main() {
-    input! {}
+    input! {
+        n: f32,
+    }
+    let (mut l, mut r) = (0.0, 100000.0);
+    while (r - l) > 0.001 {
+        let m: f32 = (l + r) / 2.0;
+        let sum = m*m*m + m;
+        if sum  >= n {
+            r = m;
+        } else {
+            l = m;
+        }
+    }
+    println!("{l}")
 }
