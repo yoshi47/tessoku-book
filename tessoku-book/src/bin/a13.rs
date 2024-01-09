5f664f75cd0b2a16ff3c18249b9ddc764c54dd1e
yoshi <yoshix47@gmail.com>
2024-01-09 15:47:18 +0900

    3.3 しゃくとり法

--- tessoku-book/src/bin/a13.rs
+++ tessoku-book/src/bin/a13.rs
@@ -1,4 +1,26 @@
 use proconio::input;
 fn main() {
-    input! {}
+    input! {
+        n : usize,
+        k: usize,
+        a: [usize; n],
+    }
+
+    let mut r = vec![0_usize; n+1];
+    for i in 0..n-1 {
+        if i == 0 {
+            r[i] = 1;
+        } else {
+            r[i] = r[i-1];
+        }
+
+        while r[i] < n && a[r[i]]-a[i] <= k {
+            r[i] += 1;
+        }
+    }
+    let mut ans = 0;
+    for i in 0..n-1 {
+        ans += r[i] - (i+1);
+    }
+    println!("{ans}");
 }
--- tessoku-book/src/bin/b13.rs
+++ tessoku-book/src/bin/b13.rs
@@ -1,4 +1,23 @@
 use proconio::input;
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize; n],
+    }
+
+    let mut asum = vec![0_usize; n+1];
+    for i in 0..n {
+        asum[i+1] = asum[i] + a[i];
+    }
+    let mut ans = 0;
+    let mut r = 0;
+    for l in 1..n+1 {
+        while r < n+1 && asum[r] - asum[l-1] <= k {
+            r += 1;
+        }
+        ans += r - l;
+    }
+
+    println!("{ans}");
 }
