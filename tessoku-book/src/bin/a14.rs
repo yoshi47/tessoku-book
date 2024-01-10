832c4c7b7147fb0ce531ad0ec019c49998bf1e71
yoshi <yoshix47@gmail.com>
2024-01-10 16:35:27 +0900

    3.4 半分全列挙

--- tessoku-book/src/bin/a14.rs
+++ tessoku-book/src/bin/a14.rs
@@ -1,4 +1,33 @@
 use proconio::input;
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize; n],
+        b: [usize; n],
+        c: [usize; n],
+        d: [usize; n],
+    }
+    let mut p = Vec::new();
+    for x in 0..n {
+        for y in 0..n {
+            p.push(a[x] + b[y]);
+        }
+    }
+    let mut q = Vec::new();
+    for x in 0..n {
+        for y in 0..n {
+            q.push(c[x] + d[y]);
+        }
+    }
+
+    q.sort();
+    for i in 0..n*n {
+        let pos1 = q.binary_search(&(k-p[i]));
+        if pos1.is_ok() {
+            println!("Yes");
+            return
+        }
+    }
+    println!("No");
 }
--- tessoku-book/src/bin/b14.rs
+++ tessoku-book/src/bin/b14.rs
@@ -1,4 +1,38 @@
 use proconio::input;
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize; n],
+    }
+    let enumerate = |x: &[usize]| {
+        let mut sum_list = Vec::new();
+        for i in 0..1 << x.len() {
+            let mut sum = 0;
+            for j in 0..x.len() {
+                if i & (1 << j) != 0 {
+                    sum += x[j];
+                }
+            }
+            sum_list.push(sum);
+        }
+        return sum_list
+    };
+
+    let (l1, l2) = a.split_at(n / 2);
+    let mut sum1 = enumerate(l1);
+    let mut sum2 = enumerate(l2);
+    sum1.sort();
+    sum2.sort();
+
+    for s1 in sum1 {
+        match sum2.binary_search(&(k - s1)) {
+            Ok(_) => {
+                println!("Yes");
+                return
+            },
+            Err(_) => continue,
+        }
+    }
+    println!("No");
 }
