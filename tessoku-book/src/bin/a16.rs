d3829d7e96ae753d859d82d32bab0b9fb8006a40
yoshi <yoshix47@gmail.com>
2024-01-11 13:28:29 +0900

    4.1 動的計画法の基本

--- tessoku-book/src/bin/a16.rs
+++ tessoku-book/src/bin/a16.rs
@@ -1,4 +1,17 @@
 use proconio::input;
+use std::cmp;
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        a: [usize; n-1],
+        b: [usize; n-2],
+    }
+    // let mut dp = Vec::new();
+    let mut dp = vec![0_usize; n];
+    dp[0] = 0;
+    dp[1] = a[0];
+    for i in 2..n {
+        dp[i] = cmp::min(dp[i-1] + a[i-1], dp[i-2] + b[i-2]);
+    }
+    println!("{}", dp[n-1]);
 }
--- tessoku-book/src/bin/b16.rs
+++ tessoku-book/src/bin/b16.rs
@@ -1,4 +1,16 @@
 use proconio::input;
+use std::cmp;
+
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        h: [isize; n],
+    }
+    let mut dp = vec![0_isize; n];
+    dp[0] = 0;
+    dp[1] = (h[0] - h[1]).abs();
+    for i in 2..n {
+        dp[i] = cmp::min(dp[i-2] + (h[i-2]-h[i]).abs(), dp[i-1] + (h[i-1]-h[i]).abs())
+    }
+    println!("{}", dp[n-1]);
 }
