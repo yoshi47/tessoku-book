5e34d90212e0cd596026a54749324c0f42ce88f9
yoshi <yoshix47@gmail.com>
2024-01-11 11:14:11 +0900

    3.5 チャレンジ問題

--- tessoku-book/src/bin/a15.rs
+++ tessoku-book/src/bin/a15.rs
@@ -1,4 +1,17 @@
 use proconio::input;
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+    let mut t = a.to_vec();
+    t.sort();
+    t.dedup();
+
+    let mut b = vec![0_usize; n];
+    for i in 0..n {
+        b[i] = t.binary_search(&a[i]).unwrap() + 1;
+    }
+    let bb: Vec<String> = b.iter().map(|&x| x.to_string()).collect();
+    println!("{}", bb.join(" "));
 }
