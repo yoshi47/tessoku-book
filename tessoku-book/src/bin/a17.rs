ce642b4879a6aaf3bc22d864b834093c8beab987
yoshi <yoshix47@gmail.com>
2024-01-11 14:47:39 +0900

    4.2 動的計画法の復元

--- tessoku-book/src/bin/a17.rs
+++ tessoku-book/src/bin/a17.rs
@@ -1,4 +1,32 @@
 use proconio::input;
+use std::cmp;
+
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        a: [usize; n-1],
+        b: [usize; n-2],
+    }
+    let mut dp = vec![0_usize; n];
+    dp[0] = 0;
+    dp[1] = a[0];
+    for i in 2..n {
+        dp[i] = cmp::min(dp[i-1] + a[i-1], dp[i-2] + b[i-2]);
+    }
+    let mut ans = Vec::new();
+    let mut place = n-1;
+    loop {
+        ans.push(place);
+        if place == 0 {break};
+
+        if dp[place-1] + a[place-1] == dp[place] {
+            place -= 1;
+        } else {
+            place -= 2;
+        }
+    }
+    println!("{}", ans.len());
+    for i in ans.iter().rev() {
+        print!("{} ", i+1);
+    }
 }
--- tessoku-book/src/bin/b17.rs
+++ tessoku-book/src/bin/b17.rs
@@ -1,4 +1,31 @@
 use proconio::input;
+use std::cmp;
+
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        h: [isize; n],
+    }
+    let mut dp = vec![0_isize; n];
+    dp[0] = 0;
+    dp[1] = (h[0] - h[1]).abs();
+    for i in 2..n {
+        dp[i] = cmp::min(dp[i-2] + (h[i-2]-h[i]).abs(), dp[i-1] + (h[i-1]-h[i]).abs())
+    }
+    let mut ans = Vec::new();
+    let mut place = n-1;
+    loop {
+        ans.push(place);
+        if place == 0 {break};
+
+        if dp[place-1] + (h[place-1] - h[place]).abs() == dp[place] {
+            place -= 1;
+        } else {
+            place -= 2;
+        }
+    }
+    println!("{}", ans.len());
+    for i in (0..ans.len()).rev() {
+        print!("{} ", ans[i]+1);
+    }
 }
