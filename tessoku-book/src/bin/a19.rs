8a7080658f123507c16013709f784184a8698933
yoshi <yoshix47@gmail.com>
2024-01-13 16:17:28 +0900

    4.4 二次元の DP（２）：ナップザック問題

--- tessoku-book/src/bin/a19.rs
+++ tessoku-book/src/bin/a19.rs
@@ -1,4 +1,29 @@
 use proconio::input;
+use std::cmp;
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        w: usize,
+        wv: [(usize, usize); n],
+    }
+
+    let mut dp = vec![vec![isize::MIN; 100001]; n+1];
+
+    dp[0][0] = 0;
+
+    for i in 1..n+1 {
+        let (w1, v1) = wv[i-1];
+        for j in 0..w+1 {
+            if j < w1 {
+                dp[i][j] = dp[i-1][j];
+            } else {
+                dp[i][j] = cmp::max(dp[i-1][j], dp[i-1][j-w1] + v1 as isize);
+            }
+        }
+    }
+    let mut ans = 0;
+    for i in 0..w+1 {
+        ans = ans.max(dp[n][i]);
+    }
+    println!("{ans}");
 }
--- tessoku-book/src/bin/b19.rs
+++ tessoku-book/src/bin/b19.rs
@@ -1,4 +1,26 @@
 use proconio::input;
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        w: isize,
+        wv: [(usize, usize); n],
+    }
+    let mut dp = vec![vec![isize::MAX; 100_009]; n+1];
+    dp[0][0] = 0;
+    for i in 1..n+1 {
+        let (ww, vv) = wv[i-1];
+        for j in 0..100_001 {
+            dp[i][j] = dp[i-1][j];
+            if j >= vv && !(dp[i-1][j-vv] == isize::MAX){
+                dp[i][j] = dp[i][j].min(dp[i-1][j-vv] + ww as isize);
+            }
+        }
+    }
+    let mut ans = 0;
+    for i in 0..100_001 {
+        if dp[n][i] <= w {
+            ans = i;
+        }
+    }
+    println!("{ans}");
 }
