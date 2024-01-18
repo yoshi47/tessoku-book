a5f7b0353fbd57cdd66f09add85287c12dbf66eb
yoshi <yoshix47@gmail.com>
2024-01-18 11:22:19 +0900

    4.5 二次元の DP（３）：最長共通部分列問題

--- tessoku-book/src/bin/a20.rs
+++ tessoku-book/src/bin/a20.rs
@@ -1,4 +1,29 @@
 use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    input! {}
+    input! {
+        s: Chars,
+        t: Chars,
+    }
+    let (ss, tt) = (s.len(), t.len());
+    let mut dp = vec![vec![0_usize; tt+1]; ss+1];
+    dp[0][0] = 0;
+    for i in 0..ss+1 {
+        for j in 0.. tt+1 {
+            if i >= 1 && j >= 1 && s[i-1] == t[j-1] {
+                dp[i][j] = *[dp[i-1][j], dp[i][j-1], dp[i-1][j-1]+1].iter().max().unwrap();
+            }
+            else if i >= 1 && j >= 1 {
+                dp[i][j] = *[dp[i-1][j], dp[i][j-1]].iter().max().unwrap();
+            }
+            else if i >= 1 {
+                dp[i][j] = dp[i-1][j];
+            }
+            else if j >= 1 {
+                dp[i][j] = dp[i][j-1];
+            }
+        }
+    }
+    println!("{}", dp[ss][tt]);
 }
--- tessoku-book/src/bin/b20.rs
+++ tessoku-book/src/bin/b20.rs
@@ -1,4 +1,26 @@
 use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    input! {}
+    input! {
+        s: Chars,
+        t: Chars,
+    }
+    let mut dp = vec![vec![0_usize; t.len()+1]; s.len()+1];
+
+    dp[0][0] = 0;
+    for i in 0..s.len()+1 {
+        for j in 0..t.len()+1 {
+            if i >= 1 && j >= 1 && s[i-1] == t[j-1] {
+                dp[i][j] = *[dp[i-1][j]+1, dp[i][j-1]+1, dp[i-1][j-1]].iter().min().unwrap();
+            } else if i >= 1 && j >= 1 {
+                dp[i][j] = *[dp[i-1][j]+1, dp[i][j-1]+1, dp[i-1][j-1]+1].iter().min().unwrap();
+            } else if i >= 1 {
+                dp[i][j] = dp[i-1][j] + 1;
+            } else if j >= 1 {
+                dp[i][j] = dp[i][j-1] + 1;
+            }
+        }
+    }
+    println!("{}", dp[s.len()][t.len()]);
 }
