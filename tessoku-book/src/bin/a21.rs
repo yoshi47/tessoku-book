51f38d33b62c602925917a0de614bdeaff6ea34d
yoshi <yoshix47@gmail.com>
2024-01-21 10:59:59 +0900

    4.6 二次元の DP（４）：区間 DP

--- tessoku-book/src/bin/a21.rs
+++ tessoku-book/src/bin/a21.rs
@@ -1,4 +1,41 @@
 use proconio::input;
+
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        pa: [(usize, usize); n],
+    }
+    let (p, a): (Vec<_>, Vec<_>) = pa.into_iter().unzip();
+
+    let mut dp = vec![vec![0; n];n];
+    dp[0][n-1] = 0;
+
+    for len in (0..n-1).rev() {
+        for l in 0..n-len {
+            let r = l + len;
+
+            let mut score1 = 0;
+            if l>=1 && l+1 <= p[l-1] && p[l-1] <= r+1 {
+                score1 = a[l-1];
+            }
+
+            let mut score2 = 0;
+            if r<=n-2 && l+1 <= p[r+1] && p[r+1] <= r+1 {
+                score2 = a[r+1];
+            }
+
+            if l == 0 {
+                dp[l][r] = dp[l][r+1] + score2;
+            } else if r == n-1 {
+                dp[l][r] = dp[l-1][r] + score1;
+            } else {
+                dp[l][r] = *[dp[l-1][r] + score1, dp[l][r+1] + score2].iter().max().unwrap();
+            }
+        }
+    }
+    let mut ans = 0;
+    for i in 0..n {
+        ans = ans.max(dp[i][i]);
+    }
+    println!("{ans}");
 }
--- tessoku-book/src/bin/b21.rs
+++ tessoku-book/src/bin/b21.rs
@@ -1,4 +1,34 @@
 use proconio::input;
+use proconio::marker::Chars;
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        s: Chars,
+    }
+
+    let mut dp = vec![vec![0; n]; n];
+    for i in 0..n {
+        dp[i][i] = 1;
+    }
+    for i in 0..n-1 {
+        if s[i] == s[i+1] {
+            dp[i][i+1] = 2;
+        } else {
+            dp[i][i+1] = 1;
+        }
+    }
+
+    for len in 2..n {
+        for l in 0..n-len {
+            let r = l + len;
+
+            if s[l] == s[r] {
+                dp[l][r] = *[dp[l][r-1], dp[l+1][r], dp[l+1][r-1]+2].iter().max().unwrap();
+            } else {
+                dp[l][r] = *[dp[l][r-1], dp[l+1][r]].iter().max().unwrap();
+            }
+        }
+    }
+
+    println!("{}", dp[0][n-1]);
 }
