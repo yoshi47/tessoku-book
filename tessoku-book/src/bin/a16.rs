4fb57184cbe18851369db9b05543269968bfff41
yoshi <yoshix47@gmail.com>
2024-02-09 16:54:09 +0900

    4.7 遷移状態の工夫

--- tessoku-book/src/bin/a16.rs
+++ tessoku-book/src/bin/a16.rs
@@ -1,17 +1,32 @@
 use proconio::input;
 use std::cmp;
+
 fn main() {
     input! {
         n: usize,
         a: [usize; n-1],
         b: [usize; n-2],
     }
-    // let mut dp = Vec::new();
-    let mut dp = vec![0_usize; n];
+    // let mut dp = vec![0_usize; n];
+    // dp[0] = 0;
+    // dp[1] = a[0];
+    // for i in 2..n {
+    //     dp[i] = cmp::min(dp[i-1] + a[i-1], dp[i-2] + b[i-2]);
+    // }
+    // println!("{}", dp[n-1]);
+
+
+    // B22
+    let mut dp = vec![20_000_000; n];
     dp[0] = 0;
     dp[1] = a[0];
-    for i in 2..n {
-        dp[i] = cmp::min(dp[i-1] + a[i-1], dp[i-2] + b[i-2]);
+    for i in 0..n {
+        if i < n-1 {
+            dp[i+1] = dp[i+1].min(dp[i]+a[i]);
+        }
+        if i < n-2 {
+            dp[i+2] = dp[i+2].min(dp[i]+b[i]);
+        }
     }
     println!("{}", dp[n-1]);
 }
--- tessoku-book/src/bin/a22.rs
+++ tessoku-book/src/bin/a22.rs
@@ -1,4 +1,18 @@
 use proconio::input;
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        a: [usize; n-1],
+        b: [usize; n-1],
+    }
+
+    let mut dp = vec![isize::MIN; n+1];
+
+    dp[1] = 0;
+    for i in 1..n {
+        dp[a[i-1]] = dp[a[i-1]].max(dp[i]+100);
+        dp[b[i-1]] = dp[b[i-1]].max(dp[i]+150);
+    }
+
+    println!("{}", dp[n]);
 }
